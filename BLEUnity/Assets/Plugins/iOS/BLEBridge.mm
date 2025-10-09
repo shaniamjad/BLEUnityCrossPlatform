09cb4dbe52f3c4d340a77ce42cd634e5c6244060
shaniamjad <shaniamjad@gmail.com>
2025-10-09 13:47:26 +0500

    Add CoreBluetooth linking to iOS postprocess

--- BLEUnity/Assets/Plugins/iOS/BLEBridge.mm
+++ BLEUnity/Assets/Plugins/iOS/BLEBridge.mm
@@ -6,9 +6,10 @@ extern "C" {
 #endif
 
 void _ble_init(const char* unityObjectName) {
-    NSString *obj = [NSString stringWithUTF8String:unityObjectName];
-    [[BLEManager shared] setUnityObjectName:obj];
-    [[BLEManager shared] start]; // initialize central manager
+    NSString *obj = [NSString stringWithUTF8String:unityObjectName ?: "UnityBLE"];
+    BLEManager *manager = [BLEManager shared];
+    manager.unityObjectName = obj;
+    [manager start];
 }
 
 void _ble_startScan() {
@@ -19,18 +20,51 @@ void _ble_stopScan() {
     [[BLEManager shared] stopScan];
 }
 
-void _ble_connect(const char* deviceId) {
-    NSString *did = [NSString stringWithUTF8String:deviceId];
-    [[BLEManager shared] connectToDevice:did];
+void _ble_connect(const char* deviceId, const char* profileJson) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    NSString *profile = profileJson ? [NSString stringWithUTF8String:profileJson] : @"";
+    [[BLEManager shared] connectToDevice:did profileJson:profile];
+}
+
+void _ble_disconnect(const char* deviceId) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    [[BLEManager shared] disconnectDevice:did];
+}
+
+void _ble_startMeasurement(const char* deviceId) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    [[BLEManager shared] startMeasurement:did];
+}
+
+void _ble_stopMeasurement(const char* deviceId) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    [[BLEManager shared] stopMeasurement:did];
+}
+
+void _ble_pauseMeasurement(const char* deviceId) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    [[BLEManager shared] pauseMeasurement:did];
+}
+
+void _ble_sendControl(const char* deviceId, const char* base64Data, const char* action) {
+    NSString *did = deviceId ? [NSString stringWithUTF8String:deviceId] : @"";
+    NSString *payload = base64Data ? [NSString stringWithUTF8String:base64Data] : @"";
+    NSString *act = action ? [NSString stringWithUTF8String:action] : @"";
+    [[BLEManager shared] sendControl:did base64Payload:payload action:act];
 }
 
 void _ble_readCharacteristic(const char* serviceUuid, const char* charUuid) {
-    [[BLEManager shared] readCharacteristicWithService:[NSString stringWithUTF8String:serviceUuid] charId:[NSString stringWithUTF8String:charUuid]];
+    NSString *service = serviceUuid ? [NSString stringWithUTF8String:serviceUuid] : @"";
+    NSString *characteristic = charUuid ? [NSString stringWithUTF8String:charUuid] : @"";
+    [[BLEManager shared] readCharacteristicWithService:service charId:characteristic];
 }
 
 void _ble_writeCharacteristic(const char* serviceUuid, const char* charUuid, const char* base64Data) {
-    NSData *data = [[NSData alloc] initWithBase64EncodedString:[NSString stringWithUTF8String:base64Data] options:0];
-    [[BLEManager shared] writeCharacteristicWithService:[NSString stringWithUTF8String:serviceUuid] charId:[NSString stringWithUTF8String:charUuid] data:data];
+    NSString *service = serviceUuid ? [NSString stringWithUTF8String:serviceUuid] : @"";
+    NSString *characteristic = charUuid ? [NSString stringWithUTF8String:charUuid] : @"";
+    NSString *b64 = base64Data ? [NSString stringWithUTF8String:base64Data] : @"";
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:b64 options:0];
+    [[BLEManager shared] writeCharacteristicWithService:service charId:characteristic data:data];
 }
 
 #ifdef __cplusplus
--- BLEUnity/Assets/Plugins/iOS/BLEManager.h
+++ BLEUnity/Assets/Plugins/iOS/BLEManager.h
@@ -3,13 +3,18 @@
 
 @interface BLEManager : NSObject <CBCentralManagerDelegate, CBPeripheralDelegate>
 
-@property (nonatomic, strong) NSString *unityObjectName;
+@property (nonatomic, copy) NSString *unityObjectName;
 
 + (instancetype)shared;
 - (void)start;
 - (void)startScan;
 - (void)stopScan;
-- (void)connectToDevice:(NSString*)deviceId;
+- (void)connectToDevice:(NSString*)deviceId profileJson:(NSString*)profileJson;
+- (void)disconnectDevice:(NSString*)deviceId;
+- (void)startMeasurement:(NSString*)deviceId;
+- (void)stopMeasurement:(NSString*)deviceId;
+- (void)pauseMeasurement:(NSString*)deviceId;
+- (void)sendControl:(NSString*)deviceId base64Payload:(NSString*)payload action:(NSString*)action;
 - (void)readCharacteristicWithService:(NSString*)service charId:(NSString*)charId;
 - (void)writeCharacteristicWithService:(NSString*)service charId:(NSString*)charId data:(NSData*)data;
 
